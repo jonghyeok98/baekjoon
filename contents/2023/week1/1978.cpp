c784d0f03287a3cc3dd3e2eb65336235410a9c76
jonghyeok98 <dlwhdgur113@naver.com>
2023-06-26 17:20:10 +0900

    find prime

--- contents/2023/week1/1978.cpp
+++ contents/2023/week1/1978.cpp
@@ -1,1 +1,35 @@
 // boj 1978 소수 찾기
+// 에라토스테네스의 체 구현
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	ios_base::sync_with_stdio(false);
+
+	vector<bool> isPrime(1001, true);
+	isPrime[0] = isPrime[1] = false;
+	for(int i = 2; i*i<=1000; i++)
+	{
+		if(isPrime[i])
+		{
+			for (int j = i * i; j <= 1000; j+=i)
+				isPrime[j] = false;
+		}
+	}
+	int N, count = 0;
+	cin >> N;
+
+	for(int i=0; i<N; i++)
+	{
+		int num;
+		cin >> num;
+		if (isPrime[num])
+			count++;
+	}
+	cout << count;
+
+	return 0;
+}
