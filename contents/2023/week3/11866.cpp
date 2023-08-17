5255c87f888684fc20613d972e4a9961ae89ced0
jonghyeok98 <dlwhdgur113@naver.com>
2023-08-17 13:34:56 +0900

    11866

--- contents/2023/week3/11866.cpp
+++ contents/2023/week3/11866.cpp
@@ -1,2 +1,47 @@
 // boj s5 11866 요세푸스 문제0
 
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, K, tmp;
+	cin >> N >> K;
+
+	queue<int> q;
+	for (int i = 1; i <= N; i++)
+		q.push(i);
+
+	cout << "<";
+
+	while(true)
+	{
+		if (q.empty())
+			break;
+		for(int i=1; i<=K; i++)
+		{
+			if(i!=K)
+			{
+				tmp = q.front();
+				q.pop();
+				q.push(tmp);
+			}
+			else
+			{
+				cout << q.front();
+				q.pop();
+				if (!q.empty())
+					cout << ", ";
+			}
+		}
+	}
+	cout << ">";
+	return 0;
+}
+
